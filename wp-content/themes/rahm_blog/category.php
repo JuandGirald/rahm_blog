a9533d4b36e40e479e5003110dfb2eba314fe384
Arkadiusz <arkadiusz_wisniewski@yahoo.com>
2014-10-06 15:18:32 -0400

    Editing category template.

--- wp-content/themes/rahm_blog/category.php
+++ wp-content/themes/rahm_blog/category.php
@@ -22,7 +22,7 @@ get_header(); ?>
 					<?php
 						global $post;
 						$category = get_the_category($post->ID);
-						echo '<h2>' . $category['name'] . "</h2>";
+						echo '<h2>' . $category->name . "</h2>";
 						echo $category[slug];
 					?>
 					<!-- <img src="/images/icon-graph.png" alt="Economic Development" /> -->
