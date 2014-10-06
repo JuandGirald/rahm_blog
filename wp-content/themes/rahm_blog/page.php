792c166d82a49e9ea3570bc48b7b5070993f8516
Arkadiusz <arkadiusz_wisniewski@yahoo.com>
2014-10-06 14:09:52 -0400

    Checking which templates to edit.

--- wp-content/themes/rahm_blog/page.php
+++ wp-content/themes/rahm_blog/page.php
@@ -15,7 +15,8 @@
 get_header(); ?>
 
 	<div id="primary" class="site-content">
-		<div id="content" role="main">index of posts
+		<div id="content" role="main">
+			<p>index of posts</p>
 
 			<?php while ( have_posts() ) : the_post(); ?>
 				<?php get_template_part( 'content', 'page' ); ?>
